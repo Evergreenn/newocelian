703e90d6abda355c91846ab09ab40758e8794b63
lyrikz <lyrikz@artifakt.io>
2017-10-07 16:31:06 +0200

    Refactor code

--- src/AppBundle/Security/TokenAuthenticator.php
+++ src/AppBundle/Security/TokenAuthenticator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AppBundle\Security;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,59 +23,26 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
      */
     private $encoderFactory;
 
+    /**
+     * TokenAuthenticator constructor.
+     *
+     * @param EncoderFactoryInterface $encoderFactoryInterface
+     */
     public function __construct(EncoderFactoryInterface $encoderFactoryInterface)
     {
         $this->encoderFactory = $encoderFactoryInterface;
     }
 
     /**
-     * Returns a response that directs the user to authenticate.
-     *
-     * This is called when an anonymous request accesses a resource that
-     * requires authentication. The job of this method is to return some
-     * response that "helps" the user start into the authentication process.
-     *
-     * Examples:
-     *  A) For a form login, you might redirect to the login page
-     *      return new RedirectResponse('/login');
-     *  B) For an API token authentication system, you return a 401 response
-     *      return new Response('Auth header required', 401);
-     *
-     * @param Request                 $request       The request that resulted in an AuthenticationException
-     * @param AuthenticationException $authException The exception that started the authentication process
-     *
-     * @return Response
+     * {@inheritdoc}
      */
-    public function start(Request $request, AuthenticationException $authException = NULL)
+    public function start(Request $request, AuthenticationException $authException = null)
     {
-            return new Response('Credentials Required', 401);
+        return new JsonResponse('Credentials Required', Response::HTTP_UNAUTHORIZED);
     }
 
     /**
-     * Get the authentication credentials from the request and return them
-     * as any type (e.g. an associate array). If you return null, authentication
-     * will be skipped.
-     *
-     * Whatever value you return here will be passed to getUser() and checkCredentials()
-     *
-     * For example, for a form login, you might:
-     *
-     *      if ($request->request->has('_username')) {
-     *          return array(
-     *              'username' => $request->request->get('_username'),
-     *              'password' => $request->request->get('_password'),
-     *          );
-     *      } else {
-     *          return;
-     *      }
-     *
-     * Or for an API token that's on a header, you might use:
-     *
-     *      return array('api_key' => $request->headers->get('X-API-TOKEN'));
-     *
-     * @param Request $request
-     *
-     * @return mixed|null
+     * {@inheritdoc}
      */
     public function getCredentials(Request $request)
     {
@@ -81,29 +50,21 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
             throw new UnauthorizedHttpException('Challenge me !');
         }
 
-        list ($basic, $credentials) = explode(' ', $header);
-        list ($email, $password) = explode(':', base64_decode($credentials));
+        list($basic, $credentials) = \explode(' ', $header);
+        if ('Basic' !== $basic) {
+            throw new BadRequestHttpException('Challenge me !');
+        }
 
-        return  [
-            'email' => $email,
+        list($email, $password) = \explode(':', \base64_decode($credentials));
+
+        return [
+            'email'    => $email,
             'password' => $password,
         ];
     }
 
     /**
-     * Return a UserInterface object based on the credentials.
-     *
-     * The *credentials* are the return value from getCredentials()
-     *
-     * You may throw an AuthenticationException if you wish. If you return
-     * null, then a UsernameNotFoundException is thrown for you.
-     *
-     * @param mixed                 $credentials
-     * @param UserProviderInterface $userProvider
-     *
-     * @throws AuthenticationException
-     *
-     * @return UserInterface|null
+     * {@inheritdoc}
      */
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
@@ -111,20 +72,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
     }
 
     /**
-     * Returns true if the credentials are valid.
-     *
-     * If any value other than true is returned, authentication will
-     * fail. You may also throw an AuthenticationException if you wish
-     * to cause authentication to fail.
-     *
-     * The *credentials* are the return value from getCredentials()
-     *
-     * @param mixed         $credentials
-     * @param UserInterface $user
-     *
-     * @return bool
-     *
-     * @throws AuthenticationException
+     * {@inheritdoc}
      */
     public function checkCredentials($credentials, UserInterface $user)
     {
@@ -134,39 +82,17 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
     }
 
     /**
-     * Called when authentication executed, but failed (e.g. wrong username password).
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the login page or a 403 response.
-     *
-     * If you return null, the request will continue, but the user will
-     * not be authenticated. This is probably not what you want to do.
-     *
-     * @param Request                 $request
-     * @param AuthenticationException $exception
-     *
-     * @return Response|null
+     * {@inheritdoc}
      */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         return new JsonResponse(
-            ['message' => $exception->getMessageKey()], Response::HTTP_FORBIDDEN);
+            ['message' => $exception->getMessageKey()], Response::HTTP_FORBIDDEN
+        );
     }
 
     /**
-     * Called when authentication executed and was successful!
-     *
-     * This should return the Response sent back to the user, like a
-     * RedirectResponse to the last page they visited.
-     *
-     * If you return null, the current request will continue, and the user
-     * will be authenticated. This makes sense, for example, with an API.
-     *
-     * @param Request        $request
-     * @param TokenInterface $token
-     * @param string         $providerKey The provider (i.e. firewall) key
-     *
-     * @return Response|null
+     * {@inheritdoc}
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
@@ -174,18 +100,7 @@ class TokenAuthenticator extends AbstractGuardAuthenticator
     }
 
     /**
-     * Does this method support remember me cookies?
-     *
-     * Remember me cookie will be set if *all* of the following are met:
-     *  A) This method returns true
-     *  B) The remember_me key under your firewall is configured
-     *  C) The "remember me" functionality is activated. This is usually
-     *      done by having a _remember_me checkbox in your form, but
-     *      can be configured by the "always_remember_me" and "remember_me_parameter"
-     *      parameters under the "remember_me" firewall key
-     *  D) The onAuthenticationSuccess method returns a Response object
-     *
-     * @return bool
+     * {@inheritdoc}
      */
     public function supportsRememberMe()
     {
