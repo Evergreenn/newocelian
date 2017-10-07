ebb0452218dfa990d84cb7ed1913d6e1db457688
lyrikz <lyrikz@artifakt.io>
2017-10-07 20:10:27 +0200

    Added user api token in login action

--- src/AppBundle/Action/Login.php
+++ src/AppBundle/Action/Login.php
@@ -5,12 +5,28 @@ namespace AppBundle\Action;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * Class Login.
  */
 final class Login
 {
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * Login constructor.
+     *
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
     /**
      * @Route(
      *     path="/login",
@@ -23,6 +39,11 @@ final class Login
      */
     public function __invoke()
     {
-        return new JsonResponse('Success');
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        return new JsonResponse([
+            'message'   => 'Login success',
+            'api_token' => $user->getApiToken(),
+        ]);
     }
 }
